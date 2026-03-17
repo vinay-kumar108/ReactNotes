1377b8e73f5a54bb52e1734380f0fd606bb1b692
vinay-kumar108 <vinay20010920@gmail.com>
2026-03-17 21:53:14 +0530

    feat: initialize 11-form-handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form >
+        <input type="text" placeholder='Enter Your Name'/>
+        <input type="email" placeholder='Enter Your Email'/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
