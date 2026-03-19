8b1b99b680fcf77a95ad4e42f0f05232cbd6afc8
vinay-kumar108 <vinay20010920@gmail.com>
2026-03-19 19:53:53 +0530

    started notes app

---
+++ 13-notes-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      for now, this is just a placeholder for the notes app. The actual implementation will be added later on 20 march 2026.
+    </div>
+  )
+}
+
+export default App
