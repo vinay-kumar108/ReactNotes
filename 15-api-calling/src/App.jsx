e8c4fcb4ea99a1177d865b9e88f12607267857ca
vinay-kumar108 <vinay20010920@gmail.com>
2026-03-22 17:44:30 +0530

    learn: API Calling & useEffect

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from'axios'
 
 const App = () => {
-    const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com');
-    const data = await response.json();
+  const [list, setList] = useState([]);
+
+/*  Calling API using fetch
+
+
+const getData= async ()=>{
+
+  
+    const res=await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data=  await res.json();
     console.log(data);
+    setList(data);
+   
+
   }
+  */
+//uisng axios
+const getData= async ()=>{
+    const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
+    setList(res.data);
+  }
+
+//useEffect Hook
+useEffect(()=>{
+  console.log('Changes occurred!')
+},[list])
+
+  
+
   return (
-    <><div >App</div>
-      <button onClick={getData}>Click me</button></>
+    <>
+    
+      <h1 >API Calling </h1>
+      <button onClick={getData}>Click Me</button>
+      {list.map((ele,idx)=>{
+        return <h2 key={ele.id} >{ele.title} {`<----->`}  {idx}</h2>
+
+      }
+
+      )
+      }
+    </>
   )
 }
 
