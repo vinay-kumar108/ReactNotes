d05b7c1cd019b3f37c8b8131c889ce929b70cb86
vinay-kumar108 <vinay20010920@gmail.com>
2026-03-23 17:19:56 +0530

    learn: Pagination

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,48 +1,67 @@
 import React, { useEffect, useState } from 'react'
-import axios from'axios'
+import axios from 'axios'
 
 const App = () => {
   const [list, setList] = useState([]);
-
-/*  Calling API using fetch
-
-
-const getData= async ()=>{
-
+  const [page, setPage] = useState(1);
   
-    const res=await fetch('https://jsonplaceholder.typicode.com/todos');
-    const data=  await res.json();
-    console.log(data);
-    setList(data);
-   
 
-  }
-  */
-//uisng axios
-const getData= async ()=>{
-    const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
-    setList(res.data);
-  }
-
-//useEffect Hook
-useEffect(()=>{
-  console.log('Changes occurred!')
-},[list])
-
+  /*  Calling API using fetch
   
+  
+  const getData= async ()=>{
+  
+    
+      const res=await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data=  await res.json();
+      console.log(data);
+      setList(data);
+     
+  
+    }
+    <--using axios-->
+    const getData = async () => {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      
+      setList(res.data);
+    }
+    */
+
+  //useEffect Hook is used to perform side effects in function components. It serves the same purpose as componentDidMount, componentDidUpdate, and componentWillUnmount in React classes, but unified into a single API.
+  // used to demonstrate pagination by fetching data from an API and updating the list based on the current page.
+  useEffect( () =>  {
+      async function setData () {
+        try{
+
+          const res= await axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`);
+          setList(res.data);
+        }
+        catch(err){
+          console.log(err);
+        }
+      };
+      setData();
+
+  }, [page])
+
+
 
   return (
     <>
-    
+
       <h1 >API Calling </h1>
-      <button onClick={getData}>Click Me</button>
-      {list.map((ele,idx)=>{
-        return <h2 key={ele.id} >{ele.title} {`<----->`}  {idx}</h2>
+<button onClick={setPage } ></button>
+      <div style={ { margin: '20px', height: '500px', overflow: 'auto' } } >{list.map((ele) => {
+        return <h2 style={{ lineHeight: '1.5' }} key={ele.id} > {ele.id}  &gt; {ele.title} </h2>
+      })}
+</div>
+      <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Prev
+      </button>
+      <button onClick={() => setPage(page + 1)}>
+        Next
+      </button>
 
-      }
-
-      )
-      }
     </>
   )
 }
